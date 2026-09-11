d0dc6257dc52fde6fe2c1db9e742b5e8dccb3a05
Vaibhav Choudhary <vaibhavchaudhary308@gmail.com>
2026-09-11 18:42:53 +0530

    resolve merge conflict

--- server.py
+++ server.py
@@ -1,10 +1,18 @@
 from fastapi import FastAPI
 
-app=FastAPI()
+app = FastAPI()
+
+students = [
+    {"id": 1, "name": "Gourav", "course": "CSE", "year": 4},
+    {"id": 2, "name": "Rahul", "course": "ECE", "year": 3},
+    {"id": 3, "name": "Ankit", "course": "CSE", "year": 4}
+]
+
 
 @app.get("/")
 def home():
     return {"message": "hello guysss welcome to rio_health"}
+
 
 @app.get("/health")
 def health():
@@ -18,20 +26,37 @@ def get_users():
         {"id": 2, "name": "Alice"}
     ]
 
+
 @app.get("/users/{user_id}")
 def get_user(user_id: int):
     users = [
         {"id": 1, "name": "John"},
         {"id": 2, "name": "Alice"}
     ]
+
     user = next((u for u in users if u["id"] == user_id), None)
     return user
+
+
+@app.get("/students")
+def students_data():
+    return students
+
+
+@app.get("/student/{student_id}")
+def get_student(student_id: int):
+    for student in students:
+        if student_id == student["id"]:
+            return student
+
+    return {"message": "Student not found"}
+
+
 @app.get("/ping")
 def ping():
     return {"status": "ok"}
 
-# 2. Dynamic Name Greeting (Path Parameter)
+
 @app.get("/greet/{name}")
 def greet_user(name: str):
     return {"message": f"Welcome to Rio Health, {name}!"}
-
