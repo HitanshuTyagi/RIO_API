e6a29d315881da01394586bcab2426d37bb59d51
Gourav Choudhary <gouravgj@gmail.com>
2026-09-11 18:11:17 +0530

    Added student requests

--- server.py
+++ server.py
@@ -2,6 +2,26 @@ from fastapi import FastAPI
 
 app=FastAPI()
 
+students = [
+    {"id": 1, "name": "Gourav", "course": "CSE", "year": 4},
+    {"id": 2, "name": "Rahul", "course": "ECE", "year": 3},
+    {"id": 3, "name": "Ankit", "course": "CSE", "year": 4}
+]
+
 @app.get("/")
 def home():
     return {"message": "hello guysss welcome to rio_health"}
+
+
+@app.get('/students')
+def students_data():
+    return students
+
+@app.get('/student/{student_id}')
+def get_student(student_id:int):
+    for student in students:
+        if student_id==student.id:
+            return student
+
+    return {"message":"Student not found"}
+
