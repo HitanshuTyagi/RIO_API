4e277f8ce59c4571d918ba3ae7f55eaa6f8ad1d2
Vaibhav Choudhary <vaibhavchaudhary308@gmail.com>
2026-09-11 17:53:57 +0530

    Add my task

--- server.py
+++ server.py
@@ -5,3 +5,15 @@ app=FastAPI()
 @app.get("/")
 def home():
     return {"message": "hello guysss welcome to rio_health"}
+
+@app.get("/health")
+def health():
+    return {"status": "ok"}
+
+
+@app.get("/users")
+def get_users():
+    return [
+        {"id": 1, "name": "John"},
+        {"id": 2, "name": "Alice"}
+    ]
